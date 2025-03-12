55d8de697f95d3314e3fc6c92d6f0213ebfedbce
alexxGmZ <algilbertgomez@gmail.com>
2025-03-12 12:17:52 +0800

    feat: generate rectangle, circle, line, and textbox

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
-import { StaticCanvas, FabricText } from "fabric"
+import { Canvas } from "fabric"
+import { generateRectangle, generateCircle, generateLine, generateTextBox } from "./object.js"
 
-const canvas = new StaticCanvas("canvas");
-const helloWorld = new FabricText('Hello world!');
-canvas.add(helloWorld);
-canvas.centerObject(helloWorld);
+const canvas = new Canvas("canvas", {
+   fireRightClick: true,
+   preserveObjectStacking: true,
+});
+
+const rectangleBtn = document.getElementById("generateRectangle")
+rectangleBtn.addEventListener("click", () => {
+   generateRectangle(canvas);
+})
+
+const circleBtn = document.getElementById("generateCircle")
+circleBtn.addEventListener("click", () => {
+   generateCircle(canvas);
+})
+
+const lineBtn = document.getElementById("generateLine")
+lineBtn.addEventListener("click", () => {
+   generateLine(canvas);
+})
+
+const textBoxBtn = document.getElementById("generateTextBox")
+textBoxBtn.addEventListener("click", () => {
+   generateTextBox(canvas);
+})
---
+++ js/object.js
@@ -0,0 +1,82 @@
+import { Rect, Circle, Line, IText } from "fabric"
+
+/**
+ * Generates a rectangle on the specified Fabric.js canvas.
+ *
+ * @param {fabric.Canvas} canvas - The Fabric.js canvas instance where the rectangle will be added.
+ */
+export function generateRectangle(canvas) {
+   if (!canvas) return;
+   console.log(`generateRectangle()`);
+   const rect = new Rect({
+      left: 100,
+      top: 100,
+      width: 50,
+      height: 50,
+      fill: "rgba(255, 255, 255, 0)",
+      stroke: "rgba(0, 0, 0, 1)",
+      strokeWidth: 2,
+      strokeUniform: true
+   });
+
+   canvas.add(rect);
+   canvas.setActiveObject(rect);
+}
+
+/**
+ * Generates a circle on the specified Fabric.js canvas.
+ *
+ * @param {fabric.Canvas} canvas - The Fabric.js canvas instance where the circle will be added.
+ */
+export function generateCircle(canvas) {
+   if (!canvas) return;
+   console.log(`generateCircle()`);
+   const circle = new Circle({
+      radius: 20,
+      left: 100,
+      top: 100,
+      fill: "rgba(255, 255, 255, 0)",
+      stroke: "rgba(0, 0, 0, 1)",
+      strokeWidth: 2,
+      strokeUniform: true
+   })
+
+   canvas.add(circle);
+   canvas.setActiveObject(circle);
+}
+
+/**
+ * Generates a line on the specified Fabric.js canvas.
+ *
+ * @param {fabric.Canvas} canvas - The Fabric.js canvas instance where the line will be added.
+ */
+export function generateLine(canvas) {
+   if (!canvas) return;
+   console.log(`generateLine()`);
+   const line = new Line([10, 50, 100, 50], {
+      stroke: "rgba(0, 0, 0, 1)",        // Line color
+      strokeWidth: 2,       // Line width
+   });
+
+   canvas.add(line);
+   canvas.setActiveObject(line);
+}
+
+/**
+ * Generates a text object on the specified Fabric.js canvas.
+ *
+ * @param {fabric.Canvas} canvas - The Fabric.js canvas instance where the text will be added.
+ */
+export function generateTextBox(canvas) {
+   if (!canvas) return;
+   console.log(`generateText()`);
+   const text = new IText("text", {
+      left: 100,
+      top: 100,
+      fontSize: 30
+   });
+
+   canvas.add(text);
+   canvas.setActiveObject(text);
+}
+
